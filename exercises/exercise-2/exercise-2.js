20f8afff73e8f2f67fb9def2308eb09e05f5a3bd
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-02-18 11:52:54 +0000

    Exercise 2 - done

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,31 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+
+function showNamesOfStudents(array) {
+  let [student1, student2, student3, student4, student5] = array;
+
+  console.log(student1.firstName + " " + student1.lastName);
+  console.log(student2.firstName + " " + student2.lastName);
+  console.log(student3.firstName + " " + student3.lastName);
+  console.log(student4.firstName + " " + student4.lastName);
+  console.log(student5.firstName + " " + student5.lastName);
+}
+
+//as for me it is more difficult to use here destructuring method if we don't know how many teachers we have in array and at which exactly position
+function showNamesOfTeachers(array) {
+  let onlyTeachers = [];
+  for (let person of array) {
+    if (person.occupation === "Teacher") {
+      onlyTeachers.push[person];
+    }
+  }
+
+  array
+    .filter((person) => person.occupation === "Teacher")
+    .forEach((person) => console.log(person.firstName + " " + person.lastName));
+}
+
+showNamesOfStudents(hogwarts);
+
+showNamesOfTeachers(hogwarts);
